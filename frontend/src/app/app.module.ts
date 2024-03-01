8d1cf2bdeea8b286d6ade94272d2a3902e8f570e
Buddhabhushan <buddhabhushanp@cybage.com>
2024-03-01 11:59:50 +0530

    Header and Footer added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { SearchComponent } from './home/search/search.component';
+import { CardsComponent } from './home/cards/cards.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    CardsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/home/cards/cards.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../service/product.service';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+
+export class CardsComponent implements OnInit {
+  
+  data=[]; 
+
+  constructor(private dataService: ProductService) { }
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe(
+      (response) => {
+        console.log("response"+response)
+        this.data = response; // Store the data received from the backend
+        console.log(this.data)
+      },
+      (error) => {
+        console.error('Error fetching data: ', error);
+      }
+    );
+  }
+}

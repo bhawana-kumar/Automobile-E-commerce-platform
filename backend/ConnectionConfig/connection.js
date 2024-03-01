c735973f6b0c398c23969b91008560e4e70f0b20
bhawana-kumar <154969532+bhawana-kumar@users.noreply.github.com>
2024-03-01 18:09:25 +0530

    Update connection.js
    Signed-off-by: bhawana-kumar <154969532+bhawana-kumar@users.noreply.github.com>

--- backend/ConnectionConfig/connection.js
+++ backend/ConnectionConfig/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose')
-const conn = mongoose.connect("mongodb://localhost:27017/database_name")
+const conn = mongoose.connect("mongodb://localhost:27017/FET")
 if(conn){
     console.log("Connected");
 }
-//new code
-// const mongoose=require('mongoose')
-// mongoose.connect('mongodb://localhost:27017/Angular_with_NodeJS_Aug22')

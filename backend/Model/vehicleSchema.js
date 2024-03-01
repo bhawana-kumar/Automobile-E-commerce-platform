1a11a319a4415bc3a9edd52a4a5d22e94135f491
bhawana-kumar <154969532+bhawana-kumar@users.noreply.github.com>
2024-03-01 18:13:58 +0530

    Update vehicleSchema.js
    Signed-off-by: bhawana-kumar <154969532+bhawana-kumar@users.noreply.github.com>

--- backend/Model/vehicleSchema.js
+++ backend/Model/vehicleSchema.js
@@ -1,15 +1,69 @@
 //Model to be added
-const mongoose=require('mongoose')
-const vehicleSchema=mongoose.Schema(
-    {
-        "productId":Number,
-        "brandName":String,
-        "modelName":String,
-        "manYear":String,
-        "images":String,
-        "engine":String,
-        "color":String,
-        "price":Number
+const mongoose = require('mongoose')
+const { number } = require('yargs')
+require('../ConnectionConfig/connection')
+
+
+const productSchema = mongoose.Schema({
+    brand: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    
+    ownership: {
+        type: String,
+        enum: ['First', 'Second','Third'], // Ensures only 'these' values are allowed
+        required: true
+    },
+
+    
+    image: {
+        data: Buffer, // Binary data of the image
+        contentType: String // Mime type of the image
+    },
+
+    color: {
+        type: String,
+        required: false
+    },
+
+    seats: {
+        type : Number,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    fuel_type: {
+        type: String,
+        required: true
+    },
+  
+    mileage: {
+        type: String,
+        required: false
+    },
+    
+    description : {
+        type: String,
+        required: false
+    },
+
+    body_type: {
+        type: String,
+        required: true
     }
-)
-module.exports=mongoose.model("products",vehicleSchema)
+});
+
+module.exports = mongoose.model("product", productSchema);

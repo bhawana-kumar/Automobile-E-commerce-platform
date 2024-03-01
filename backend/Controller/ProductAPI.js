33ceb3e03a8599d1db4afde942468d7cb2f43d66
bhawana-kumar <154969532+bhawana-kumar@users.noreply.github.com>
2024-03-01 18:12:01 +0530

    Update VehicleApi.js
    Signed-off-by: bhawana-kumar <154969532+bhawana-kumar@users.noreply.github.com>

--- backend/Controller/ProductAPI.js
+++ backend/Controller/ProductAPI.js
@@ -1,37 +1,28 @@
-//Controller to be added
-const mongoose=require('mongoose')
-const express=require('express')
-const cors=require('cors')
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
 require('../ConnectionConfig/connection')
-const productModel=require('../Model/vehicleSchema')
 
-const app=express();
-app.use(express.json())
-app.use(cors())
-app.post("/insertProduct",async(req,resp)=>{
-    const product=new productModel(req.body);
-    console.log(product+" product is ");
-    console.log(req.body+" product is ");
-    const result=await product.save();
-    resp.send(result)
+
+const productModel = require('../Model/VehicleModel');
+const { request } = require('http');
+
+const ex = express();
+ex.use(express.json())
+ex.use(cors()) 
+
+
+ex.post('/addProduct',async(request,response)=>{
+    const product = new productModel(request.body);
+    const result = await product.save();
+    response.send(result)
 })
 
-app.get("/getProduct",async(req,resp)=>{
-    const result=await productModel.find();
-    resp.send(result);
-})
-
-app.delete("/deleteProduct/:pid",async (req,resp)=>{
-    // console.log(req.params.pid);
-    const result=await productModel.deleteOne(
-        {'productId':req.params.pid}
-        )
-        resp.send(result)
+ex.get('/getProduct',async(request,response)=>{
+    const result = await productModel.find();
+    response.send(result)
 })
 
 
-app.listen(4000,function(err){
-    if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", 4000);
-}
-   )
+ex.listen(4000)
+
